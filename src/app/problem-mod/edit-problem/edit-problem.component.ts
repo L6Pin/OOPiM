fc1421d387f2798d91a3a18b8ee08910e6e802a2
Luka Peric <lukaperickv@gmail.com>
2022-03-15 21:26:50 +0100

    commit

--- src/app/problem-mod/edit-problem/edit-problem.component.ts
+++ src/app/problem-mod/edit-problem/edit-problem.component.ts
@@ -5,6 +5,7 @@ import { ApiService } from 'src/app/api.service';
 import { FormControl, FormGroup } from '@angular/forms';
 import { problemsFacade } from 'src/app/Store/Facade/problems.facade';
 import { Observable } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-edit-problem',
@@ -15,57 +16,55 @@ export class EditProblemComponent implements OnInit {
   constructor(
     private api: ApiService,
     private route: ActivatedRoute,
-    private facade: problemsFacade
-    
+    private facade: problemsFacade,
+    private router: Router
   ) {}
 
-  editForm!: FormGroup
-  problemData: any
-  readonly user$: Observable<any> = this.facade.loggedUser$
-    
-  
-  ngOnInit(): void {    
+  editForm!: FormGroup;
+  problemData: any;
+  readonly user$: Observable<any> = this.facade.loggedUser$;
+
+  ngOnInit(): void {
     this.editForm = new FormGroup({
-      problem_name: new FormControl(""),
-      description: new FormControl(""),
-      room: new FormControl(""),
-      comment: new FormControl(""),
-      status_id: new FormControl(""),
+      problem_name: new FormControl(''),
+      description: new FormControl(''),
+      room: new FormControl(''),
+      comment: new FormControl(''),
+      status_id: new FormControl(''),
     });
 
     this.api
       .getSingleProblem(this.route.snapshot.params.id)
-      .subscribe((res:any) => {
+      .subscribe((res: any) => {
         this.editForm.setValue({
           problem_name: res.problem_name,
           description: res.description,
           room: res.room,
           comment: res.comment,
-          status_id: res.status_id
-        })
-      })
+          status_id: res.status_id,
+        });
+      });
   }
 
-  editProblem(){
-    this.api.patchSingleProblem({
-      id: Number(this.route.snapshot.params.id),
-      ...this.editForm.value, 
-      status_id: Number(this.editForm.value.status_id),
-      category_id: 1,
-      priority_id: 1,
-      supplier_id: 4,
-      worker_id: 3,
-      staff_id: 8
-    }).subscribe(res => console.log({
-      id: Number(this.route.snapshot.params.id),
-      ...this.editForm.value, 
-      status_id: Number(this.editForm.value.status_id),
-      category_id: 1,
-      priority_id: 1,
-      supplier_id: 4,
-      worker_id: 3,
-      staff_id: 8
-    }))
+  editProblem(user: any) {
+    this.api
+      .patchSingleProblem({
+        id: Number(this.route.snapshot.params.id),
+        ...this.editForm.value,
+        status_id: Number(this.editForm.value.status_id),
+        category_id: 1,
+        priority_id: 1,
+        supplier_id: 4,
+        worker_id: 3,
+        staff_id: 8,
+      })
+      .subscribe((res) => {
+        let role!: string;
+        if (user.is_admin) role = 'admin';
+        if (user.is_worker) role = 'worker';
+        if (user.is_supplier) role = 'supplier';
+
+        this.router.navigate([`/profile/${role}`]);
+      });
   }
 }
-
